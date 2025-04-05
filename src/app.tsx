47b2463d7b1b8f5fa77984c87d32170a86a1e643
Leonardo Maciel <leonardomacielj@gmail.com>
2025-04-05 13:57:19 -0300

    feat: added axios in project

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import Form from "./components/Form/Form";
+import ToDoList from "./components/ToDoList/ToDoList";
+import "./index.scss";
 
 export default function App() {
   return (
     <section>
-      <h1>Todos</h1>
+      <h1 className="teste">todos</h1>
+
+      <div className="toDo-container">
+        <div className="shadow" />
+        <Form />
+
+        <ToDoList />
+        <div className="small-shadow" />
+      </div>
     </section>
   );
 }
